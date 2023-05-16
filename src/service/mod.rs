aba1d569e5ba5237d2561d7cbae7e635064195e2
Thinking-Dragon <cfmgp@hotmail.com>
2023-05-16 15:40:09 -0400

    Add user service

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,2 +1,3 @@
 pub mod event_service;
 pub mod trip_service;
+pub mod user_service;
---
+++ src/service/user_service.rs
@@ -0,0 +1,29 @@
+use crate::model::user::User;
+
+#[derive(Clone, Copy)]
+pub struct UserService;
+
+impl UserService {
+    pub fn get_users(self) -> Vec<User> {
+        let users: Vec<User> = Vec::new();
+        return users;
+    }
+
+    pub fn get_user(self, user_id: String) -> User {
+        let user: User = User {
+            id: "".to_string(),
+            name: "User".to_string(),
+            password: "123".to_string(),
+            picture_url: "https://123.com/user/123".to_string()
+        };
+        return user;
+    }
+
+    pub fn create_user(self, user: User) {
+
+    }
+
+    pub fn update_user(self, user_id: String, user: User) {
+
+    }
+}
